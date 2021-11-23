b58f9454e2d088083075c387212e987cccfbcfe4
Waldo-byte <69579496+Waldo-byte@users.noreply.github.com>
2021-11-23 03:06:20 +0200

    front end commit

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './components.css';
+
+function HeaderComponent() {
+    return (
+        <div className = "header">
+            <title />
+            
+        </div>
+    )
+}
+
+export default HeaderComponent
